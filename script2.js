c2c339ffdced2a73fdedf876722dd58c781d8c25
ceciliam1-design <ccricerocks1@gmail.com>
2026-09-13 21:39:06 -0400

    Add shopping list functionality to script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,8 @@
+console.log("This is javascript page 2");
+const shoppingList = [];
+shoppingList.push("Bread", "Milk", "Eggs", "Cookies", "Cheese");
+shoppingList[2] = "Can of Peas";
+shoppingList.splice(0, 2);
+shoppingList.reverse();
+console.log("The list contains 6 items");
 
